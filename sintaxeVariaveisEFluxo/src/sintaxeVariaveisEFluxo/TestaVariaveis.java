d6833a641debe77a8b6a9f33456f7995d7da8983
Queren Hapuque <queren.fernandes@yahoo.com>
2023-02-19 18:29:34 -0300

    variaveis

---
+++ sintaxeVariaveisEFluxo/src/sintaxeVariaveisEFluxo/TestaVariaveis.java
@@ -0,0 +1,24 @@
+package sintaxeVariaveisEFluxo;
+
+public class TestaVariaveis {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("Ola novo teste");
+		
+		int idade;
+		idade = 28;
+		
+		System.out.println(idade);
+		
+		idade = 10 + 18;
+		
+		System.out.println(idade);
+		
+		idade = 5 * 5 + 3;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade e " + idade + " parabens!");
+	}
+}
