6fc9893472d05f1abb9add9e1bb9d8850c3a3e71
Queren Hapuque <queren.fernandes@yahoo.com>
2023-02-22 08:49:48 -0300

    criando metodo de transferir

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -20,4 +20,14 @@ public class Conta {
 			return false;
 		}
 	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -10,5 +10,26 @@ public class TestaMetodo {
 		boolean conseguiuRetirar = contaDoOdin.saca(20);
 		System.out.println(contaDoOdin.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		
+		Conta contaDaNoemy = new Conta();
+		contaDaNoemy.saldo = 0;
+		
+		contaDaNoemy.deposita(5000);
+		
+		if(contaDaNoemy.transfere(500, contaDoOdin)) {
+			System.out.println("transferencia com sucesso");
+			System.out.println("o novo saldo da conta do Odin"
+					+ " e: " + contaDoOdin.saldo);
+			System.out.println("o novo saldo da conta da Noemy"
+					+ " e: " + contaDaNoemy.saldo);
+			
+		}else {
+			System.out.println("transferencia nao realizada, "
+					+ "sem saldo disponivel");
+		}
+		
+	
+		
 	}
 }
